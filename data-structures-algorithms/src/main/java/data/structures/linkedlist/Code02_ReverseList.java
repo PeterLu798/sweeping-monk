1acdf0651fb8ac5721d67a725a849bed16108a4d
PeterLu <1649675756@qq.com>
2024-11-13 16:34:32 +0800

    单链表增删改查

--- data-structures-algorithms/src/main/java/data/structures/linkedlist/Code02_ReverseList.java
+++ data-structures-algorithms/src/main/java/data/structures/linkedlist/Code02_ReverseList.java
@@ -1,4 +1,116 @@
 package data.structures.linkedlist;
 
 public class Code02_ReverseList {
+
+    //定义一个带头节点的单链表，所谓带头节点就是头节点不存放数据，只包含一个指向下一个节点的指针（同理可知不带头节点就是链表的第一个节点存放数据）
+    final Node head = new Node(null);
+
+    public static class Node {
+        public Integer value;
+        public Node next;
+
+        public Node(Integer value) {
+            this.value = value;
+        }
+    }
+
+    //单链表增：在链表尾部增加节点
+    public Node add(Node add) {
+        if (add == null) {
+            return head;
+        }
+        Node t = head;
+        while (t.next != null) {
+            t = t.next;
+        }
+        t.next = add;
+        return head;
+    }
+
+    //头插入
+    public Node addHead(Node add) {
+        if (add == null) {
+            return head;
+        }
+        Node t = head.next;
+        head.next = add;
+        add.next = t;
+        return head;
+    }
+
+    //单链表删
+    public Node remove(Integer value) {
+        if (head.next == null) {
+            return null;
+        }
+        Node t = head;
+        Node remove = null;
+        while (t.next != null) {
+            if (t.next.value.equals(value)) {
+                remove = t.next;
+                t.next = t.next.next;
+                break;
+            }
+            t = t.next;
+        }
+        return remove;
+    }
+
+    //单链表改
+    public boolean update(Integer value, Integer updateValue) {
+        if (head.next == null) {
+            return false;
+        }
+        Node t = head;
+        boolean isSuc = false;
+        while (t.next != null) {
+            if (t.next.value.equals(value)) {
+                isSuc = true;
+                t.next.value = updateValue;
+                break;
+            }
+            t = t.next;
+        }
+        return isSuc;
+    }
+
+    //单链表查
+
+    //打印单链表
+    public void list() {
+        Node t = head;
+        while (t.next != null) {
+            System.out.println(t.next.value);
+            t = t.next;
+        }
+    }
+
+    //反转一个单链表
+
+
+    public static void main(String[] args) {
+        Code02_ReverseList list = new Code02_ReverseList();
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        list.add(node1);
+        list.add(node2);
+        list.add(node3);
+        list.add(node4);
+        list.add(node5);
+        Node node6 = new Node(6);
+        list.addHead(node6);
+        list.list();
+
+        Node remove = list.remove(8);
+        System.out.println("删除的节点是：" + (remove != null ? remove.value : "null"));
+        list.list();
+
+        list.update(6, 0);
+        list.update(5, 50);
+        System.out.println("-------------");
+        list.list();
+    }
 }
--- data-structures-algorithms/src/main/java/data/structures/linkedlist/Code04_IsPalindromeList.java
+++ data-structures-algorithms/src/main/java/data/structures/linkedlist/Code04_IsPalindromeList.java
@@ -54,4 +54,54 @@ public class Code04_IsPalindromeList {
         }
         return true;
     }
+
+    public static boolean isPalindrome3(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        //快慢指针，当n2走到末尾的时候，n1走到中间
+        Node n1 = head;
+        Node n2 = head;
+        while (n2.next != null && n2.next.next != null) {
+            n1 = n1.next;
+            n2 = n2.next.next;
+        }
+        //此时n1指向中间节点，n2指向末尾节点
+
+        //将链表的后半部分反转
+        //将n2指向中间节点的next
+        n2 = n1.next;
+        //将中间节点的next指向null
+        n1.next = null;
+        Node n3 = null;
+        while (n2 != null) {
+            n3 = n2.next;
+            n2.next = n1;
+            n1 = n2;
+            n2 = n3;
+        }
+        //
+        n3 = n1;
+        n2 = head;
+        boolean res = true;
+        while (n1 != null && n2 != null) {
+            if (n1.value != n2.value) {
+                res = false;
+                break;
+            }
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+
+        //将链表恢复
+        n1 = n3.next;
+        n3.next = null;
+        while (n1 != null) {
+            n2 = n1.next;
+            n1.next = n3;
+            n3 = n1;
+            n1 = n2;
+        }
+        return res;
+    }
 }
