3297c4fdd2d2e2e163548432e3da7dddebddf39d
PeterLu <1649675756@qq.com>
2024-11-25 17:08:40 +0800

    full binary tree

--- data-structures-algorithms/src/main/java/data/structures/binarytree/Code04_FullBinaryTree.java
+++ data-structures-algorithms/src/main/java/data/structures/binarytree/Code04_FullBinaryTree.java
@@ -1,5 +1,44 @@
 package data.structures.binarytree;
 
 public class Code04_FullBinaryTree {
+    public class Node {
+        public Integer value;
+        public Node left;
 
+        public Node right;
+
+        public Node(Integer value) {
+            this.value = value;
+        }
+    }
+
+    //判断一颗二叉树是不是满二叉树
+    public boolean isFBT(Node head) {
+        Info info = process(head);
+        return info.nodes == (1 << info.height - 1); // 2 ^ (info.height)等价于 1 << info.height
+
+    }
+
+    public class Info {
+        public int height;
+        public int nodes;
+
+        public Info(int height, int nodes) {
+            this.height = height;
+            this.nodes = nodes;
+        }
+    }
+
+    public Info process(Node head) {
+        if (head == null) {
+            return new Info(0, 0);
+        }
+        Info left = process(head.left);
+        Info right = process(head.right);
+
+        int height = Math.max(left.height, right.height) + 1;
+        int nodes = left.nodes + right.nodes + 1;
+
+        return new Info(height, nodes);
+    }
 }
