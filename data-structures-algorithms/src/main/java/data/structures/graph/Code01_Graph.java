b727a7fbed8f3a5bb29d151b953ec456807a95b0
PeterLu <1649675756@qq.com>
2024-12-19 21:50:15 +0800

    graph code

--- data-structures-algorithms/src/main/java/data/structures/graph/Code01_Graph.java
+++ data-structures-algorithms/src/main/java/data/structures/graph/Code01_Graph.java
@@ -1,5 +1,9 @@
 package data.structures.graph;
 
+import org.jgrapht.alg.util.UnionFind;
+
+import java.util.*;
+
 public class Code01_Graph {
     public Graph createGraph(Integer[][] matrix) {
         Graph graph = new Graph();
@@ -23,5 +27,132 @@ public class Code01_Graph {
             graph.edges.add(newEdge);
         }
         return graph;
+    }
+
+    //从node出发，进行宽度优先遍历
+    public void bfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        //为了防止环形结构造成遍历死循环
+        Set<Node> set = new HashSet<>();
+        queue.add(node);
+        set.add(node);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            System.out.println(cur.value);
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    queue.add(next);
+                    set.add(node);
+                }
+            }
+        }
+    }
+
+    //从node出发，进行深度优先遍历
+    public void dfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        //为了防止环形结构造成遍历死循环
+        Set<Node> set = new HashSet<>();
+        stack.push(node);
+        set.add(node);
+        System.out.println(node.value);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    System.out.println(next.value);
+                    break;
+                }
+            }
+        }
+    }
+
+    //拓扑排序算法
+    public List<Node> sortedTopology(Graph graph) {
+        //key: 某个node， value: 剩余的入度
+        HashMap<Node, Integer> inMap = new HashMap<>();
+        //入度为0的点才能进入这个队列
+        Queue<Node> zeroInQueue = new LinkedList<>();
+        for (Node node : graph.nodes.values()) {
+            inMap.put(node, node.in);
+            if (node.in == 0) {
+                zeroInQueue.add(node);
+            }
+        }
+        //拓扑排序的结果，加入到result中
+        List<Node> result = new ArrayList<>();
+        while (!zeroInQueue.isEmpty()) {
+            Node cur = zeroInQueue.poll();
+            result.add(cur);
+            for (Node node : cur.nexts) {
+                inMap.put(node, inMap.get(node) - 1);
+                if (inMap.get(node) == 0) {
+                    zeroInQueue.add(node);
+                }
+            }
+        }
+        return result;
+    }
+
+    public class EdgeComparator implements Comparator<Edge> {
+
+        @Override
+        public int compare(Edge o1, Edge o2) {
+            return o1.weight - o2.weight;
+        }
+    }
+
+    //kruskal算法
+    public Set<Edge> kruskalMST(Graph graph) {
+        UnionFind unionFind = new UnionFind(new HashSet(graph.nodes.values()));
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(new EdgeComparator());
+        for (Edge edge : graph.edges) {
+            priorityQueue.add(edge);
+        }
+        Set<Edge> result = new HashSet<>();
+        while (!priorityQueue.isEmpty()) {
+            Edge edge = priorityQueue.poll();
+            if (unionFind.inSameSet(edge.from, edge.to)) {
+                result.add(edge);
+                unionFind.union(edge.from, edge.to);
+            }
+        }
+        return result;
+    }
+
+    //prim算法
+    public Set<Edge> primMST(Graph graph) {
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(new EdgeComparator());
+        Set<Node> set = new HashSet<>();
+        Set<Edge> result = new HashSet<>();
+        for (Node node : graph.nodes.values()) {
+            if (!set.contains(node)) {
+                set.add(node);
+                for (Edge edge : node.edges) {
+                    priorityQueue.add(edge);
+                }
+                while (!priorityQueue.isEmpty()) {
+                    Edge edge = priorityQueue.poll();
+                    Node toNode = edge.to;
+                    if (!set.contains(toNode)) {
+                        set.add(toNode);
+                        result.add(edge);
+                        for (Edge nextEdge : toNode.edges) {
+                            priorityQueue.add(nextEdge);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
